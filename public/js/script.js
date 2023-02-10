9614b568c60b20d5ab8266f291b83e6c07a1327f
ryannthegr8 <mmbaleryan@gmail.com>
2023-02-11 01:44:15 +0300

    share-link

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,20 @@
+//animation of the share button
 const shareBtn = document.querySelector(".share-btn");
 const shareOptions = document.querySelector(".share-options");
 
 shareBtn.addEventListener("click", () => {
   shareOptions.classList.toggle("active");
 });
+
+//copy to clipboard
+function copyFunction() {
+  const copyText = document.getElementById("myInput").textContent;
+  const textArea = document.createElement("textarea");
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  cpylink.innerText = "Text copied";
+  textArea.remove();
+}
+document.getElementById("cpylink").addEventListener("click", copyFunction);
